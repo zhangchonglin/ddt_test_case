7f143798bc5965b67852dc52b0436d001ad43769
Chonglin Zhang <zhangchonglin@users.noreply.github.com>
2023-05-12 13:20:04 -0400

    Update submit_1node_np=4.sh

--- submit_1node_np=4.sh
+++ submit_1node_np=4.sh
@@ -13,5 +13,5 @@
 module load PrgEnv-gnu
 module load arm-forge
 
-ddt --offline -o offline.html -trace-at=sub,i,a\(1\) -break-at=offline.f:23,5:3:2 srun -n 4 ./offline
+ddt --offline -o offline.html -trace-at=sub,i,a\(1\) -break-at=offline.f90:23,5:3:2 srun -n 4 ./offline
 
